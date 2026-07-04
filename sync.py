847c5d2255a9b464a00575c99a8522c8b0b15fb2
Cir9no-0001 <stanley8box@gmail.com>
2026-07-03 21:32:18 -0400

    Remove redundant URL variable in sync.py

--- sync.py
+++ sync.py
@@ -3,22 +3,19 @@ import requests
 
 username = os.environ["LEETCODE_USERNAME"]
 
-url = "https://leetcode.com/graphql"
-
 query = {
     "query": """
     query recentAcSubmissions($username: String!) {
       recentAcSubmissionList(username: $username) {
         title
         titleSlug
-        timestamp
       }
     }
     """,
     "variables": {"username": username}
 }
 
-r = requests.post(url, json=query)
+r = requests.post("https://leetcode.com/graphql", json=query)
 data = r.json()
 
 print(data)
