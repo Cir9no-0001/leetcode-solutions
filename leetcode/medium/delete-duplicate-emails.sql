8a741f75cfbdaf47cdc8852a04524476963918bb
Cir9no-0001 <stanley8box@gmail.com>
2026-07-04 00:09:09 -0400

    Delete leetcode/medium directory

--- leetcode/medium/delete-duplicate-emails.sql
+++
@@ -1,8 +0,0 @@
--- Delete Duplicate Emails
--- https://leetcode.com/problems/delete-duplicate-emails/
-
-# Write your MySQL query statement below
-Delete d
-from Person p
-inner join Person d
-    on p.email=d.email and p.id<d.id;
--- leetcode/medium/rising-temperature.sql
+++
@@ -1,8 +0,0 @@
--- Rising Temperature
--- https://leetcode.com/problems/rising-temperature/
-
-Select w.id as Id
-from Weather w
-inner join Weather q
-on DATEDIFF(w.recordDate, q.recordDate) = 1
-where w.temperature>q.temperature;
