1184abf79812cf7ecdb52e0bfb86b81ef67fffa4
Cir9no-0001 <stanley8box@gmail.com>
2026-07-04 00:02:31 -0400

    Delete leetcode/easy directory

--- leetcode/easy/employees-earning-more-than-their-managers.sql
+++
@@ -1,4 +0,0 @@
--- Employees Earning More Than Their Managers
--- https://leetcode.com/problems/employees-earning-more-than-their-managers/
-
--- SQL code not auto-retrievable from LeetCode API reliably
