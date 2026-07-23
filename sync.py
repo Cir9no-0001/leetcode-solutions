786f5b37e4e1ab096d334f53c8d5bfe97693f815
SC <stanley8box@gmail.com>
2026-07-22 21:21:37 -0400

    Fix typo and improve README note clarity

--- sync.py
+++ sync.py
@@ -415,7 +415,9 @@ Last updated: {stats["last_updated"]}
 
 ## Author's Note
 - The hints are the notes btw :>
-- Repo meant for SQL LeetCode questions until language recognition is implemented, did two sum by accident pls ignore for now!
+- Repo meant for SQL LeetCode questions until language recognition is implemented; did two sum by accident, pls ignore for now!
+- Repo is based on my alt: leetcode.com/u/C1rn0_Fum0/
+- Lmk if my questions solved counter ever breaks!
 """
 
 with open("README.md", "w", encoding="utf-8") as f:
